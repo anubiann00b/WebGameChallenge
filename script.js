9c268049b081fe6429c8342cd3db24621ae9d181
Shreyas <skraman1999@gmail.com>
2014-08-10 23:23:00 -0700

    Simple adding text.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function update() {
-    document.getElementById('textarea').innerHTML += "Hi";
+    addText(document.getElementById('entry').value);
+    refocus();
+}
+
+function addText(text) {
+    document.getElementById('textarea').innerHTML += text;
+}
+
+function refocus() {
+    document.getElementById('entry').value = '';
+    document.getElementById('entry').focus();
 }
